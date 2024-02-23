0be3fcc9b2edf59ce714d9939a74476b41b3fce8
Mads Als Andersen <126453479+MadsAls@users.noreply.github.com>
2024-02-23 20:47:51 +0100

    time line

---
+++ js/main.js
@@ -0,0 +1,19 @@
+  window.onload = function () {
+    //initialize swiper when document ready
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'vertical',
+        loop: true,
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
+  };
